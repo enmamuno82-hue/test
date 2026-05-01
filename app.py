109e8abcb5e87874fb0fb61360a8ebb12c97f234
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-01 22:01:41 +0000

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 @st.cache_data
 def load_data():
     sheet_ID = "133p_AZdXgB3YUstAlOrn25tOJuplWbuGkx9KYwW9O7M"
-    url = f"https://docs.google.com/spreadsheets/d/133p_AZdXgB3YUstAlOrn25tOJuplWbuGkx9KYwW9O7M/export?format=csv"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_ID}/export?format=csv"
 
     data = pd.read_csv(url)
     return data
