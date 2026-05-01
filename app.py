935e18143f9a6acf97a34e5d8ef7a1639f483103
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-01 22:41:51 +0000

    update

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 
 @st.cache_data
-def load_data():
+def load_data(gid):
     sheet_ID = "133p_AZdXgB3YUstAlOrn25tOJuplWbuGkx9KYwW9O7M"
-    url = f"https://docs.google.com/spreadsheets/d/{sheet_ID}/export?format=csv"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_ID}/export?format=csv&gid={gid}"
 
     data = pd.read_csv(url)
     return data
 
-df = load_data()
+df = load_data(0)
 
 st.write("Test data", df)
