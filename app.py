daee3b082c5fc3c3f9fbe29cb43016b34abd1d5c
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-01 17:44:35 +0000

    inputed data

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import streamlit as st
-st.write("Hello World! This is my stat tracker.")
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    sheet_ID = 133p_AZdXgB3YUstAlOrn25tOJuplWbuGkx9KYwW9O7M
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_ID}/export?format=csv"
+
+    data = pd.read_cvs(url)
+    return data
+
+df = load_data()
+
+st.write("Test data", df)
