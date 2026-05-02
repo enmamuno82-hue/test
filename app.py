b7526977857dbe38b28c87749c1d600810a2d832
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-02 00:11:30 +0000

    iu

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def load_data(gid):
 def create_standings(data):
     standings = data.groupby(['Player', 'Outcome']).size().unstack(fill_value=0)
     #checks if collumn is empty
-    standings = standings['Player', 'w', 'l', 't']
+    standings = standings[['Player', 'w', 'l', 't']]
     standings = standings.sort_values(by='w', ascending=False)
     return standings.reset_index()
 
