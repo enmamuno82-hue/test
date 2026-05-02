54c3bde6dcaeb0614496a47ae210c9323aa6eb47
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-02 20:19:45 +0000

    f

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def create_standings(data, pdata):
     standings = standings[col].reset_index()
 
     standings = standings.merge(pdata[['PlayerID', 'Name']], on='PlayerID', how='left')
-    standings['Player'] = standings['Name'] + "\n" + "(" + standings['PlayerID'].astype(str) + ")"
+    standings['Player'] = standings['PlayerID'].astype(str) + " " + standings['Name']
 
     col.remove('PlayerID')
     standings['GP'] = standings[col].sum(axis=1)
