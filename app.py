6a36cb89d5b12792c4956930ff4180d732b1f0bf
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-02 19:07:35 +0000

    col

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def create_standings(data, pdata):
 
     standings = standings.merge(pdata[['PlayerID', 'Name']], on='PlayerID', how='left')
     standings['Player'] = standings['Name'] + "\n(" + standings['PlayerID'].astype(str) + ")"
+    final_cols = ['Player'] + col
 
     standings = standings.sort_values(by='w', ascending=False)
     return standings.reset_index(drop=True)
