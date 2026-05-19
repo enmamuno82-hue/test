b47b9b2cf1f30bb2be18a2745a1692eacce2fcf7
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-19 14:18:58 +0000

    f

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def player_profile(data, pdata):
             st.title(f"👤 {player_name}")
 
         with col2:
-            st.title(pdata['Age'].iat[0].astype(str) + "yrs")
+            st.header(pdata['Age'].iat[0].astype(str) + "yrs")
 
     else:
         st.error("Player not found.")
