b2c8cf47e15b65298a28a5cd91b75aabb7567e40
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-21 14:55:33 +0000

    f

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def player_profile(data, pdata):
     
         data = data[data['PlayerID'].astype(str) == str(pid)]
         st.subheader("Achievements")
-        ach = data.groupby(['Season', 'Game']).reset_index()
+        ach = data.groupby(['Season', 'Game']).size()
         st.write(ach)
 
 
