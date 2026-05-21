574020115133331b5ee20ee5db0e719c433b5f09
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-21 13:42:40 +0000

    c

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def player_profile(data, pdata):
         
         col.remove("Color")
         stats['GP'] = stats[col].sum(axis=1)
-        stats['Win %'] = ((stats['Wins'] / stats['GP']).round() * 100).astype(str) + "%"
+        stats['Win %'] = ((stats['Wins'] / stats['GP']) * 100).round().astype(str) + "%"
 
         st.dataframe(stats,hide_index=True)
 
