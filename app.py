0612197066f63589aebbb0b5b2fcb5d0b405f046
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-26 13:05:46 +0000

    f

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def player_profile(data, pdata):
         ach = plays['Game'].tolist().count("playoffs")
         
         w1 = data[data['Game'] == "playoffs"]
-        wins = plays['Outcome'].tolist().count("w")
+        wins = w1['Outcome'].tolist().count("w")
         st.write(wins)
 
 
