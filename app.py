ea4af6abb7ea00a16a2a2996b1ec1f71da11ddf6
enmamuno82-hue <enmamuno82@gmail.com>
2026-05-28 12:33:50 +0000

    f

--- app.py
+++ app.py
@@ -133,13 +133,13 @@ def player_profile(data, pdata):
         st.write(achi)
 
         for i in range(2):
-            #if i == 0:
-            st.subheader("All Time Statistics")
-            stats = data.groupby(['Color', 'Outcome']).size().unstack(fill_value=0).reset_index()
-            #else:
-                #st.subheader("Season Statistics")
-                #d2 = find_seas(data)
-                #stats = d2.groupby(['Color', 'Outcome']).size().unstack(fill_value=0).reset_index()
+            if i == 0:
+                st.subheader("All Time Statistics")
+                stats = data.groupby(['Color', 'Outcome']).size().unstack(fill_value=0).reset_index()
+            else:
+                st.subheader("Season Statistics")
+                d2 = find_seas(data)
+                stats = d2.groupby(['Color', 'Outcome']).size().unstack(fill_value=0).reset_index()
 
         
 
