8fa2d458c368b2e7ba3dff0469b50a0ee68cef19
enmamuno82-hue <enmamuno82@gmail.com>
2026-06-01 14:32:56 +0000

    r

--- app.py
+++ app.py
@@ -137,9 +137,6 @@ def player_profile(data, pdata, ofdata):
         semis = ofdata1['Matches'].tolist().count(2) + ofdata2['Matches'].tolist().count(2)
         finals = ofdata1['Matches'].tolist().count(3) + ofdata2['Matches'].tolist().count(3)
         
-        st.write(finals)
-        st.write(ofdata1)
-        st.write(ofdata2)
 
         ach = {
             "Playoff Wins": [wins],
